1604cc86b7e23816dcba9c29f4d1992dc30e3e93
Psychopatz <bpatrickoliverb@gmail.com>
2024-12-16 23:36:08 +0800

    Update uvicorn initialization

--- backend/main.py
+++ backend/main.py
@@ -61,4 +61,4 @@ if __name__ == "__main__":
 
     port = int(os.environ.get("PORT", 8000))  # Get port from env or default
     print(f"Application Port: {port}")
-    uvicorn.run(app, host="0.0.0.0", port=port, reload=True) 
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
