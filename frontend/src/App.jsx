f8a7b976ea0f12ba8ad89cfa126334639cb56a47
Psychopatz <bpatrickoliverb@gmail.com>
2024-12-18 04:38:25 +0800

    Initialize frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,35 @@
-// App.js
-import React, { useState } from 'react';
-import EpubExtractor from './components/EpubExtractor';
-import EpubViewer from './components/EpubViewer';
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
-
-const App = () => {
-  const [content, setContent] = useState([]);
-  console.log(content);
-
-  const handleExtract = (extractedContent) => {
-    setContent(extractedContent);
-  };
+function App() {
+  const [count, setCount] = useState(0)
 
   return (
-    <div>
-      <EpubExtractor onExtract={handleExtract} />
-      <EpubViewer content={content} />
-    </div>
-  );
-};
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
 
-export default App;
+export default App
